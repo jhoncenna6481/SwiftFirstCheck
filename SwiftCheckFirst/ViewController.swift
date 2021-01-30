317c87ffe38044a00e4d22d66136d1acaec0e8bb
BizMIS <support@cellica.com>
2021-01-30 14:07:09 +0530

    30 Jan 2021

--- SwiftCheckFirst/ViewController.swift
+++ SwiftCheckFirst/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+ 
+    @IBOutlet weak var lblName: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        lblName.text = "Hello XYZ"
     }
 
 
