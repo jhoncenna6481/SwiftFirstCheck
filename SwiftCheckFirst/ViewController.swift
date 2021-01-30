b598ec680b2ebb75b8ce5ac0844128c2fee3d101
BizMIS <support@cellica.com>
2021-01-30 13:58:10 +0530

    Initial Commit

---
+++ SwiftCheckFirst/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftCheckFirst
+//
+//  Created by Admin on 30/01/21.
+//  Copyright © 2021 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
