4fd48b64e75a7aae52aab43eee91dba5c8cd2637
BizMIS <support@cellica.com>
2021-01-30 14:15:20 +0530

    30 Jan Test

--- SwiftCheckFirst/ViewController.swift
+++ SwiftCheckFirst/ViewController.swift
@@ -16,8 +16,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         lblName.text = "Hello XYZ"
+        
+        changeText()
+        
+        changeText1()
     }
 
+    func changeText ()
+    {
+        lblName.text = "Hello XYZ"
+        
+         for i in 1...100 {
+             //outputs Hello world for 100 times in the screen
+             print("Hello, World! %d", i)
+         }
+    }
+    
+    func changeText1 ()
+    {
+        lblName.text = "Hello XYZ"
+        
+         for i in 1...100 {
+             //outputs Hello world for 100 times in the screen
+             print("Hello, World! %d", i)
+         }
+    }
 
 }
 
